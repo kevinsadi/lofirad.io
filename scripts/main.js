d88a3013c24c475c5fed853a48aad67dbe23bbfe
PXLante <34428034+PXLante@users.noreply.github.com>
2019-12-13 23:55:31 -0500

    updating js
    Almost working if soundcloud decided to work with us. Can scrape songs from playlists, but can't actually stream the music

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
+console.log(SC);
+
 SC.initialize({
     client_id: 'r4nH5X72hWzUcXFiXFCBs275NbNMSF8Y',
 });
 
+SC.get('/playlists/944951920').then(function(playlist) {
+    playlist.tracks.forEach(function(track) {
+      console.log(track.id);
+    });
+});
 
-SC.get('/user/183/tracks').then(function(tracks){
-    alert('Latest track: ' + tracks[0].title);
+console.log('/tracks/304632798');
+
+SC.stream('/tracks/337223917').then(function(player){
+    player.play().then(function(){
+      console.log('Playback started!');
+    }).catch(function(e){
+      console.error('Playback rejected. Try calling play() from a user interaction.', e);
+    });
   });
 
-console.log('f in the fucking chat boys');
+// console.log('f in the fucking chat boys');
