2fdf659efcca3aa6eaa958fa6b8891fb72aa45c9
PXLante <34428034+PXLante@users.noreply.github.com>
2019-12-14 15:03:27 -0500

    More js
    sorting js file. Adding event listeners for subgenre buttons.

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,83 @@
+// variables
+var currentQueue = [];
+var currentSong; //int
+
+
+
+
+// initializing
 console.log(SC);
 
 SC.initialize({
     client_id: 'r4nH5X72hWzUcXFiXFCBs275NbNMSF8Y',
 });
 
-SC.get('/playlists/944951920').then(function(playlist) {
-    playlist.tracks.forEach(function(track) {
-      console.log(track.id);
-    });
-});
 
-console.log('/tracks/304632798');
 
-SC.stream('/tracks/337223917').then(function(player){
-    player.play().then(function(){
-      console.log('Playback started!');
-    }).catch(function(e){
-      console.error('Playback rejected. Try calling play() from a user interaction.', e);
-    });
+// get playlist functions
+
+function getChill(){
+  SC.get('/playlists/944939650').then(function(playlist) {
+      for (var i = 0; i < playlist.tracks.length; i++){
+        currentQueue[i] = playlist.tracks[i];
+      }
   });
+}
+
+function getJazzy(){
+  SC.get('/playlists/944951920').then(function(playlist) {
+      for (var i = 0; i < playlist.tracks.length; i++){
+        currentQueue[i] = playlist.tracks[i];
+      }
+  });
+}
+
+function getVocalFree(){
+  SC.get('/playlists/368429987').then(function(playlist) {
+      for (var i = 0; i < playlist.tracks.length; i++){
+        currentQueue[i] = playlist.tracks[i];
+      }
+  });
+}
+
+console.log(currentQueue);
+
+
+// play function
+
+// SC.stream('/tracks/337223917').then(function(player){
+//     player.play().then(function(){
+//       console.log('Playback started!');
+//     }).catch(function(e){
+//       console.error('Playback rejected. Try calling play() from a user interaction.', e);
+//     });
+//   });
 
 // console.log('f in the fucking chat boys');
+
+
+// Adding event listeners for subgenres
+var vocalFree = document.getElementById('vocalfree');
+console.log(vocalFree);
+
+vocalFree.addEventListener('click', ()=>{
+  getVocalFree();
+  currentSong = currentQueue[0];
+  console.log(currentSong);
+});
+
+
+
+
+
+
+
+//trash so I don't forget whats happening
+
+// //jazzy
+// SC.get('/playlists/944951920').then(function(playlist) {
+//   console.log(playlist.tracks);
+//   playlist.tracks.forEach(function(track) {
+//     // console.log(track);
+//   });
+// });
